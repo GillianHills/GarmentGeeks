973740c168f272e7f91a2efe48d8d957a892a090
GillianHills <48174193+GillianHills@users.noreply.github.com>
2019-03-03 14:22:07 +0000

    Update privileges.sql

--- Ex19/privileges.sql
+++ Ex19/privileges.sql
@@ -1,6 +1,18 @@
 //Finance mgr//
-
 GRANT SELECT, INSERT, UPDATE ON garmentgeeks.customers,garmentgeeks.stock, garmentgeeks.suppliers, garmentgeeks.currentbookings,
 garmentgeeks.pastbookings 
+TO 'user-name'@'localhost' IDENTIFIED BY 'password';
+
+//stock mgr//
+GRANT SELECT, INSERT, UPDATE ON garmentgeeks.stock, garmentgeeks.suppliers, garmentgeeks.currentbookings, garmentgeeks.pastbookings
+garmentgeeks.membership 
+TO 'user-name'@'localhost' IDENTIFIED BY 'password';
+
+//customer service team//
+GRANT SELECT, INSERT, UPDATE ON garmentgeeks.customers, garmentgeeks.membership, garmentgeeks.currentbookings, garmentgeeks.pastbookings  
+TO 'user-name'@'localhost' IDENTIFIED BY 'password';
+
+//customer//
+GRANT SELECT, INSERT, UPDATE ON garmentgeeks.customers, garmentgeeks.stock  
 TO 'user-name'@'localhost' IDENTIFIED BY 'password';
 
