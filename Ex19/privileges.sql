026eb87795a171033cf9f839f073c9c6c4ac6361
GillianHills <48174193+GillianHills@users.noreply.github.com>
2019-03-03 14:16:30 +0000

    Create privileges.sql

---
+++ Ex19/privileges.sql
@@ -0,0 +1,6 @@
+//Finance mgr//
+
+GRANT SELECT, INSERT, UPDATE ON garmentgeeks.customers,garmentgeeks.stock, garmentgeeks.suppliers, garmentgeeks.currentbookings,
+garmentgeeks.pastbookings 
+TO 'user-name'@'localhost' IDENTIFIED BY 'password';
+
